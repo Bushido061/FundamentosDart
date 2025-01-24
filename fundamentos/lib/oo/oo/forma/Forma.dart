3cf83e6c5266be5f01e3e764d884b243ca084923
bushido061 <huds291@gmail.com>
2025-01-23 22:25:16 -0300

    novo banco de dados

---
+++ fundamentos/lib/oo/oo/forma/Forma.dart
@@ -0,0 +1,17 @@
+import 'enum.dart';
+
+abstract class Forma{
+  tpForma tipoForma;
+
+  static int idFormaClasse =0;
+  int idFormaInstancia = 0;
+
+  Forma( this.tipoForma );
+
+  double calculaArea();
+
+  void imprimeForma(){     
+      print('${tipoForma.name} com área de ${calculaArea()} <-> ID da forma: Classe ${idFormaClasse} Instância ${idFormaInstancia}');
+
+}
+}
---
+++ fundamentos/lib/oo/oo/forma/Quadrado.dart
@@ -0,0 +1,17 @@
+import 'Forma.dart';
+import 'enum.dart';
+
+class Quadrado extends Forma{
+  double lado = 0;
+
+  Quadrado( this.lado ) : super(tpForma.Quadrado){
+  Forma.idFormaClasse = 1;
+  super.idFormaInstancia =1;
+  }
+ 
+  @override
+  double calculaArea(){
+    return lado * lado;
+  }
+
+}
